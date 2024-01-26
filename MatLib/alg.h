0ac04f1006298fd2b411713b4665b8a9d6c850a5
chenzhaojie1997@foxmail.com <chenzhaojie1997@foxmail.com>
2024-01-27 04:59:06 +0800

    matlib

---
+++ MatLib/alg.h
@@ -0,0 +1,7 @@
+#include "Mat.h"
+
+// biliner interpolate a single-channel image
+void BilinerInterpolate(uchar* outData, const int outWidth, const int outHeight,
+                        uchar* inData,  const int inWidth,  const int inHeight, 
+                        float scale);
+
--- ch2/alg.h
+++
@@ -1,4 +0,0 @@
-#include "Mat.h"
-
-// biliner interpolate a single-channel image
-void BilinerInterpolate(uchar* outData,  uchar* inData, const int width, const int height, float scale);
